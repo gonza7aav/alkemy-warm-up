cdf258ad62c95d09812e99166657a4e289c02b7d
Aguirre Gonzalo Adolfo <gonza.a.aguirre@gmail.com>
2021-11-22 05:03:15 -0300

    fixs database names in config/config.js

--- config/config.js
+++ config/config.js
@@ -2,14 +2,14 @@ module.exports = {
   development: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    database: `${process.env.DATABASE_NAME}-dev`,
+    database: `${process.env.DATABASE_NAME}_dev`,
     host: process.env.DATABASE_HOST,
     dialect: 'mysql',
   },
   test: {
     username: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
-    database: `${process.env.DATABASE_NAME}-test`,
+    database: `${process.env.DATABASE_NAME}_test`,
     host: process.env.DATABASE_HOST,
     dialect: 'mysql',
     logging: false,
